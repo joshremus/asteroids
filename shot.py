1052e9123712a3315f6e9a0872f4783d84f9dca0
Josh Remus <josh@swm-its.com>
2024-12-15 22:20:45 -0500

    shot fixed I guess

--- shot.py
+++ shot.py
@@ -1,43 +1,13 @@
 import pygame
 from constants import *
-from circleshape import *
+from circleshape import CircleShape
 
 class Shot(CircleShape):
-
-
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
-
-        #Asteroid.containers = (asteroids, updatable, drawable)
-
-    def draw(position, screen):
-        pygame.draw.circle(screen, "white", position, SHOT_RADIUS, 2)
-
-    def update(self, dt):
-        #forward = pygame.Vector2
-        self.position = self.position + (self.velocity * dt)
-
-    def main():
-        shots = pygame.sprite.Group
-        
-        
-
-
-
-'''
-import pygame
-from constants import *
-from circleshape import *
-
-class Shot(CircleShape):
-
-
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
 
-        shots = pygame.sprite.Group
-        
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
-'''
+    def update(self, dt):
+        self.position += self.velocity * dt
